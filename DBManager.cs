30c0c7ef9a06f780a574be8d94695b7a33ac045b
hdseventh <hdseventh.hq@gmail.com>
2021-10-21 03:26:56 +0700

    Added Claim and List command.

--- DBManager.cs
+++ DBManager.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Data;
 using System.IO;
-
 using TShockAPI;
 using TShockAPI.DB;
-using Mono.Data.Sqlite;
-using MySql.Data.MySqlClient;
 
 namespace VoucherSystem
 {
@@ -51,7 +49,20 @@ namespace VoucherSystem
             try
             {
                 return db.Query("INSERT INTO VoucherSystem (SerialNumber, Reward, CreatedBy, ClaimedBy, Expiration) " +
-                    "VALUES (@0, @1, @2, @3, @4, @5)", serialnum, reward, author, claimer, expire) != 0;
+                    "VALUES (@0, @1, @2, @3, @4)", serialnum, reward, author, claimer, expire) != 0;
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+            return false;
+        }
+
+        public bool UpdateVoucher(int Id, string claimer)
+        {
+            try
+            {
+                return db.Query("UPDATE VoucherSystem SET ClaimedBy=@1 WHERE Id=@0", Id, claimer) != 0;
             }
             catch (Exception ex)
             {
@@ -73,5 +84,80 @@ namespace VoucherSystem
             return false;
         }
 
+        public List<VoucherSystems> GetVoucherList()
+        {
+            try
+            {
+                // list all rows in database
+                List<VoucherSystems> vlist = new List<VoucherSystems>();
+                using (var reader = db.QueryReader("SELECT * FROM VoucherSystem WHERE ClaimedBy=\"\""))
+                {
+                    while (reader.Read())
+                    {
+                        vlist.Add(new VoucherSystems(reader.Get<int>("Id"), reader.Get<string>("SerialNumber"),
+                            reader.Get<string>("Reward"), reader.Get<string>("CreatedBy"), reader.Get<string>("ClaimedBy"), reader.Get<string>("Expiration")));
+                    }
+                }
+                return vlist;
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+
+            return new List<VoucherSystems>();
+        }
+
+        public VoucherSystems ClaimVoucher(string check)
+        {
+            try
+            {
+                // search for matching range in database
+                List<VoucherSystems> vlist = GetVoucherList();
+                foreach (VoucherSystems voucher in vlist)
+                {
+                    if (check == voucher.SerialNumber)
+                        return voucher;
+                }
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+
+            return null;
+        }
+
+    }
+
+    public class VoucherSystems
+    {
+        public int Id { get; set; }
+        public string SerialNumber { get; set; }
+        public string Reward { get; set; }
+        public string CreatedBy { get; set; }
+        public string ClaimedBy { get; set; }
+        public string Expiration { get; set; }
+
+        // class constructors
+        public VoucherSystems(int Id, string serial, string reward, string author, string claimer, string expire)
+        {
+            this.Id = Id;
+            this.SerialNumber = serial;
+            this.Reward = reward;
+            this.CreatedBy = author;
+            this.ClaimedBy = claimer;
+            this.Expiration = expire;
+        }
+
+        public VoucherSystems()
+        {
+            this.Id = 0;
+            this.SerialNumber = "";
+            this.Reward = "";
+            this.CreatedBy = "";
+            this.ClaimedBy = "";
+            this.Expiration = "";
+        }
     }
 }
--- Main.cs
+++ Main.cs
@@ -1,94 +1,161 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
+using Wolfje.Plugins.SEconomy;
+using Wolfje.Plugins.SEconomy.Journal;
 
 namespace VoucherSystem
 {
-	[ApiVersion(2, 1)]
-	public class VoucherSystem : TerrariaPlugin
-	{
-		public override string Author => "hdseventh";
-		public override string Description => "Voucher system for TShock";
-		public override string Name => "Voucher System";
-		public override Version Version{ get { return new Version(1, 0, 0, 0); } }
-		public VoucherSystem(Main game) : base(game) { }
+    [ApiVersion(2, 1)]
+    public class VoucherSystem : TerrariaPlugin
+    {
+        public override string Author => "hdseventh";
+        public override string Description => "Voucher system for TShock";
+        public override string Name => "Voucher System";
+        public override Version Version { get { return new Version(1, 0, 0, 0); } }
+        public VoucherSystem(Main game) : base(game) { }
 
-		private DBManager vsystem;
-		public override void Initialize()
-		{
-			ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
-		}
-
-		public void OnInitialize(EventArgs args)
+        private DBManager vsystem;
+        public override void Initialize()
         {
-			vsystem = new DBManager();
-			Commands.ChatCommands.Add(new Command("vsystem.use", VoucherSystemUse, "voucher"));
-		}
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
 
-		private void VoucherSystemUse(CommandArgs args)
+        public void OnInitialize(EventArgs args)
         {
-			TSPlayer player = args.Player;
-			if (args.Parameters.Count < 1)
-			{
-				args.Player.SendInfoMessage("{0}voucher claim <serialnumber> - Claim a voucher.", Commands.Specifier);
-				if (args.Player.HasPermission("vsystem.admin"))
-				{
-					args.Player.SendInfoMessage("{0}voucher add <serialnumber> <reward> <expiration> - Add a voucher.", Commands.Specifier);
-					args.Player.SendInfoMessage("{0}voucher del <voucherid> - Delete a voucher.", Commands.Specifier);
-				}
-				return;
-			}
-			string subcmd = args.Parameters[0].ToLower();
-			switch (subcmd)
+            vsystem = new DBManager();
+            Commands.ChatCommands.Add(new Command("vsystem.use", VoucherSystemUse, "voucher"));
+        }
+
+        async void VoucherSystemUse(CommandArgs args)
+        {
+            TSPlayer player = args.Player;
+            if (args.Parameters.Count < 1)
             {
-				case "claim":
-					//claim placeholder
-					break;
-				case "add":
-					if (args.Parameters.Count < 3)
-					{
-						player.SendErrorMessage("Invalid syntax! Proper syntax: {0}voucher add <serialnumber> <reward> <expiration>", Commands.Specifier);
-						return;
-					}
-					DateTime expiration = DateTime.MaxValue;
-					if (TShock.Utils.TryParseTime(args.Parameters[4], out int seconds))
-					{
-						expiration = DateTime.UtcNow.AddSeconds(seconds);
-					}
-
-					if (vsystem.AddVoucher(args.Parameters[1], args.Parameters[2], args.Player.Name, "", expiration))
-					{
-						player.SendSuccessMessage("Successfully added a new voucher.");
-					}
-					else
-					{
-						player.SendErrorMessage("Failed adding the new voucher. Check logs for more info.");
-					}
-					break;
-				case "del":
-					if (args.Parameters.Count < 2)
-					{
-						player.SendErrorMessage("Invalid syntax! Proper syntax: {0}voucher del <voucherid>", Commands.Specifier);
-						return;
-					}
-					if (vsystem.DeleteVoucher(args.Parameters[1]))
-						player.SendSuccessMessage("Deleted voucher with ID {0}.", args.Parameters[1]);
-					else
-						player.SendErrorMessage("Failed deleting voucher with ID {0}. Check logs for more info.", args.Parameters[1]);
-					break;
+                args.Player.SendInfoMessage("{0}voucher claim <serialnumber> - Claim a voucher.", Commands.Specifier);
+                if (args.Player.HasPermission("vsystem.admin"))
+                {
+                    args.Player.SendInfoMessage("{0}voucher add <serialnumber> <reward> <expiration> - Add a voucher.", Commands.Specifier);
+                    args.Player.SendInfoMessage("{0}voucher del <voucherid> - Delete a voucher.", Commands.Specifier);
+                    args.Player.SendInfoMessage("{0}voucher list - Shows the voucher list.", Commands.Specifier);
+                }
+                return;
             }
+            string subcmd = args.Parameters[0].ToLower();
+            switch (subcmd)
+            {
+                case "claim":
+                    if (args.Parameters.Count < 2)
+                    {
+                        player.SendErrorMessage("Invalid syntax! Proper syntax: {0}voucher claim <serialnumber>", Commands.Specifier);
+                        return;
+                    }
+                    VoucherSystems vlist = vsystem.ClaimVoucher(args.Parameters[1]);
+                    if (vlist == null)
+                    {
+                        player.SendErrorMessage("Voucher code is not valid!");
+                        return;
+                    }
 
-		}
+                    if (!vsystem.UpdateVoucher(vlist.Id, player.Name))
+                    {
+                        player.SendErrorMessage("Error while claiming the voucher. Contact Admin for more information.");
+                        return;
+                    }
 
-		protected override void Dispose(bool Disposing)
-		{
-			if (Disposing)
-			{
-				ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
-			}
-			base.Dispose(Disposing);
-		}
-	}
+                    if (SEconomyPlugin.Instance != null)
+                    {
+                        var playerBankAccount = SEconomyPlugin.Instance.GetBankAccount(player);
+                        await SEconomyPlugin.Instance.WorldAccount.TransferToAsync(
+                            playerBankAccount,
+                            int.Parse(vlist.Reward),
+                            BankAccountTransferOptions.AnnounceToReceiver,
+                            "Claimed a Voucher.",
+                            "Claiming a voucher with voucher ID:" + vlist.Id);
+                    }
+
+                    break;
+                case "add":
+                    if (args.Parameters.Count < 3)
+                    {
+                        player.SendErrorMessage("Invalid syntax! Proper syntax: {0}voucher add <serialnumber> <reward> <expiration>", Commands.Specifier);
+                        return;
+                    }
+                    DateTime expiration = DateTime.MaxValue;
+                    if (TShock.Utils.TryParseTime(args.Parameters[3], out int seconds))
+                    {
+                        expiration = DateTime.UtcNow.AddSeconds(seconds);
+                    }
+
+                    if (!int.TryParse(args.Parameters[2], out int result))
+                    {
+                        player.SendErrorMessage("Invalid reward format! Numerical-only is the proper format.");
+                        return;
+                    }
+
+                    if (vsystem.AddVoucher(args.Parameters[1], args.Parameters[2], args.Player.Name, "", expiration))
+                    {
+                        player.SendSuccessMessage("Successfully added a new voucher.");
+                    }
+                    else
+                    {
+                        player.SendErrorMessage("Failed adding the new voucher. Check logs for more info.");
+                    }
+                    break;
+                case "del":
+                    if (args.Parameters.Count < 2)
+                    {
+                        player.SendErrorMessage("Invalid syntax! Proper syntax: {0}voucher del <voucherid>", Commands.Specifier);
+                        return;
+                    }
+                    if (vsystem.DeleteVoucher(args.Parameters[1]))
+                        player.SendSuccessMessage("Deleted voucher with ID {0}.", args.Parameters[1]);
+                    else
+                        player.SendErrorMessage("Failed deleting voucher with ID {0}. Check logs for more info.", args.Parameters[1]);
+                    break;
+                case "list":
+                    // integrate pagination tool
+                    int pagenumber;
+                    if (!PaginationTools.TryParsePageNumber(args.Parameters, 1, args.Player, out pagenumber))
+                        return;
+
+                    // fetch only ranges from the list
+                    List<VoucherSystems> list = vsystem.GetVoucherList();
+                    var namelist = from vouchers in list
+                                   select vouchers.SerialNumber;
+
+
+                    // show data from user's specified page
+                    PaginationTools.SendPage(player, pagenumber, PaginationTools.BuildLinesFromTerms(namelist),
+                            new PaginationTools.Settings
+                            {
+                                HeaderFormat = "Vouchers List ({0}/{1}):",
+                                FooterFormat = "Type {0}voucher list {{0}} for more.".SFormat(Commands.Specifier),
+                                NothingToDisplayString = "There are currently no voucher listed."
+                            });
+                    break;
+                default:
+                    args.Player.SendInfoMessage("{0}voucher claim <serialnumber> - Claim a voucher.", Commands.Specifier);
+                    if (args.Player.HasPermission("vsystem.admin"))
+                    {
+                        args.Player.SendInfoMessage("{0}voucher add <serialnumber> <reward> <expiration> - Add a voucher.", Commands.Specifier);
+                        args.Player.SendInfoMessage("{0}voucher del <voucherid> - Delete a voucher.", Commands.Specifier);
+                        args.Player.SendInfoMessage("{0}voucher list - Shows the voucher list.", Commands.Specifier);
+                    }
+                    break;
+            }
+        }
+
+        protected override void Dispose(bool Disposing)
+        {
+            if (Disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+            }
+            base.Dispose(Disposing);
+        }
+    }
 }
