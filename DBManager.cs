2ef99405b6aebd6dfe6c151136f967f22e6491fe
hdseventh <hdseventh.hq@gmail.com>
2021-10-21 00:54:40 +0700

    Add project files.

---
+++ DBManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.IO;
+
+using TShockAPI;
+using TShockAPI.DB;
+using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
+
+namespace VoucherSystem
+{
+    class DBManager
+    {
+        private IDbConnection db;
+        public DBManager()
+        {
+            switch (TShock.Config.Settings.StorageType.ToLower())
+            {
+                case "mysql":
+                    string[] host = TShock.Config.Settings.MySqlHost.Split(':');
+                    db = new MySqlConnection()
+                    {
+                        ConnectionString = String.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+                        host[0],
+                        host.Length == 1 ? "3306" : host[1],
+                        TShock.Config.Settings.MySqlDbName,
+                        TShock.Config.Settings.MySqlUsername,
+                        TShock.Config.Settings.MySqlPassword)
+                    };
+                    break;
+                case "sqlite":
+                    string dbPath = Path.Combine(TShock.SavePath, "VoucherSystem.sqlite");
+                    db = new SqliteConnection(String.Format("uri=file://{0},Version=3", dbPath));
+                    break;
+            }
+            SqlTableCreator creator = new SqlTableCreator(db, db.GetSqlType() == SqlType.Sqlite ?
+                (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+            creator.EnsureTableStructure(new SqlTable("VoucherSystem",
+                new SqlColumn("Id", MySqlDbType.Int32) { Primary = true, AutoIncrement = true },
+                new SqlColumn("SerialNumber", MySqlDbType.Text),
+                new SqlColumn("Reward", MySqlDbType.Text),
+                new SqlColumn("CreatedBy", MySqlDbType.Text),
+                new SqlColumn("ClaimedBy", MySqlDbType.Text),
+                new SqlColumn("Expiration", MySqlDbType.Text)));
+        }
+
+        public bool AddVoucher(string serialnum, string reward, string author, string claimer, string expire)
+        {
+            try
+            {
+                return db.Query("INSERT INTO VoucherSystem (SerialNumber, Reward, CreatedBy, ClaimedBy, Expiration) " +
+                    "VALUES (@0, @1, @2, @3, @4, @5)", serialnum, reward, author, claimer, expire) != 0;
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+            return false;
+        }
+
+        public bool DeleteVoucher(string id)
+        {
+            try
+            {
+                return db.Query("DELETE FROM VoucherSystem WHERE Id = @0", id) != 0;
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace VoucherSystem
+{
+	[ApiVersion(2, 1)]
+	public class VoucherSystem : TerrariaPlugin
+	{
+		public override string Author => "hdseventh";
+		public override string Description => "Voucher system for TShock";
+		public override string Name => "Voucher System";
+		public override Version Version{ get { return new Version(1, 0, 0, 0); } }
+		public VoucherSystem(Main game) : base(game) { }
+
+		private DBManager vsystem;
+		public override void Initialize()
+		{
+			ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+		}
+
+		public void OnInitialize(EventArgs args)
+        {
+			vsystem = new DBManager();
+			Commands.ChatCommands.Add(new Command("vsystem.use", VoucherSystemUse, "voucher"));
+		}
+
+		private void VoucherSystemUse(CommandArgs args)
+        {
+			TSPlayer player = args.Player;
+			if (args.Parameters.Count < 1)
+			{
+				args.Player.SendInfoMessage("{0}voucher claim <serialnumber> - Claim a voucher.", Commands.Specifier);
+				if (args.Player.HasPermission("vsystem.admin"))
+				{
+					args.Player.SendInfoMessage("{0}voucher add - Add a voucher.", Commands.Specifier);
+					args.Player.SendInfoMessage("{0}voucher del <voucherid> - Delete a voucher.", Commands.Specifier);
+				}
+				return;
+			}
+
+			string subcmd = args.Parameters[0].ToLower();
+			switch (subcmd)
+            {
+				case "claim":
+					//claim placeholder
+					return;
+				case "add":
+					//add placeholder
+					return;
+				case "del":
+					//del placeholder
+					return;
+            }
+
+		}
+
+		protected override void Dispose(bool Disposing)
+		{
+			if (Disposing)
+			{
+				ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+			}
+			base.Dispose(Disposing);
+		}
+	}
+}
